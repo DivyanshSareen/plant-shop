59e5b54d1f066bc4da228b12e1261837b29c35d3
DivyanshSareen <sareendivyansh@gmail.com>
2022-03-30 17:18:22 +0530

    created Home-page under page section
    added Nav component to HomePage page

---
+++ src/components/Nav.jsx
@@ -0,0 +1,40 @@
+export default function Nav(){
+    return(<>
+        <nav>
+        <div class="nav-wrapper">
+            <a href="./index.html">
+                <div class="nav-logo">
+                    <i class="fa-solid fa-store">
+                    </i> Ecom Store
+                </div>
+            </a>
+            <div class="nav-list">
+                <div class="nav-item input search">
+                    <label for="search"><i class="fa-solid fa-magnifying-glass"></i>  </label>
+                    <input class="input-data" type="text" id="search" placeholder="Search Products"></input>
+                </div>
+                <a href="./login.html">
+                    <button class="nav-item btn btn-ghost login-btn">
+                        <i class="fa-solid fa-arrow-right-to-bracket"></i>
+                        Login
+                    </button>
+                </a>
+                <a href="./wishlist.html">
+                    <div class="nav-item icon-badge btn btn-ghost">
+                        <i class="fa-regular fa-heart icon">
+                            <div class="badge-num">6</div>
+                        </i>
+                    </div>
+                </a>
+                <a href="./cart.html">
+                    <div class="nav-item icon-badge btn btn-ghost">
+                        <i class="fa-solid fa-cart-shopping icon">
+                            <div class="badge-num">6</div>
+                        </i>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </nav>
+    </>);
+}
