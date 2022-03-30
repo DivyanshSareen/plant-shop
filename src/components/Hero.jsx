d3ba2234c2628238306e6490b541ec8185b43310
DivyanshSareen <sareendivyansh@gmail.com>
2022-03-30 17:22:16 +0530

    added Hero component to HomePage.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+export default function Hero(){
+    return(<section>
+        <div class="hero">
+            <h1 class="hero-title h1"> Ecom Store </h1>
+            <div class="hero-subtitle subtitle1"> We do sell something </div>
+        </div>
+    </section>);
+}
