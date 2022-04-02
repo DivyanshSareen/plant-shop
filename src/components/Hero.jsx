8aaeeffeacefc04951ce92f507abb11b20d2b8fa
DivyanshSareen <sareendivyansh@gmail.com>
2022-04-02 12:19:49 +0530

    used prettier, added spacing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,10 @@
-export default function Hero(){
-    return(<section>
-        <div class="hero">
-            <h1 class="hero-title h1"> Ecom Store </h1>
-            <div class="hero-subtitle subtitle1"> We do sell something </div>
-        </div>
-    </section>);
+export default function Hero() {
+  return (
+    <section>
+      <div class='hero'>
+        <h1 class='hero-title h1'> Ecom Store </h1>
+        <div class='hero-subtitle subtitle1'> We do sell something </div>
+      </div>
+    </section>
+  );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,12 @@ import Nav from "../components/Nav";
 import Hero from "../components/Hero";
 import CategoryGrid from "../components/CategoryGrid";
 
-export default function HomePage(){
-    return(
-        <>
-            <Nav />
-            <Hero />
-            <CategoryGrid />
-        </>
-    );
+export default function HomePage() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <CategoryGrid />
+    </>
+  );
 }
