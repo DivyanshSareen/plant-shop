6bfb8a8c3e94c272679796cc93ce444e652cbc69
DivyanshSareen <sareendivyansh@gmail.com>
2022-04-03 20:19:29 +0530

    removed temporary routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import { Link } from "react-router-dom";
-import Nav from "./components/Nav";
 import "./styles/common.css";
 
 function App() {
-  return (
-    <div className='App'>
-      <Nav />
-      <Link to='/cart'>Cart</Link>
-      {" || "}
-      <Link to='/login'>Login</Link>
-      {" || "}
-      <Link to='/signup'>signup</Link>
-      {" || "}
-      <Link to='/wishlist'>wishlist</Link>
-      {" || "}
-      <Link to='/listing'>listing</Link>
-      {" || "}
-      <Link to='/home'>home</Link>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
