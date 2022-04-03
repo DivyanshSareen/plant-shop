48127dacf7216f047d110dfbe8920b6135a55e4b
DivyanshSareen <sareendivyansh@gmail.com>
2022-04-03 20:29:45 +0530

    added routes for all the pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { Routes, Route } from "react-router-dom";
 import "./styles/common.css";
+import Nav from "./components/Nav";
+import CartPage from "./pages/CartPage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import WishlistPage from "./pages/WishlistPage";
+import ProductListings from "./pages/ProductListings";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='cart' element={<CartPage />} />
+        <Route path='login' element={<LoginPage />} />
+        <Route path='signup' element={<SignupPage />} />
+        <Route path='listing' element={<ProductListings />} />
+        <Route path='wishlist' element={<WishlistPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
