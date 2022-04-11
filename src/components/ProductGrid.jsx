d37c92cf5092cfe7ae0edfde6ce3ca87f89b703b
DivyanshSareen <sareendivyansh@gmail.com>
2022-04-11 18:07:47 +0530

    replaced useState with useReducer in product-listing page

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -2,11 +2,12 @@ import ProductCard from "./ProductCard";
 import { useProduct } from "../context/product-context";
 
 export default function ProductGrid() {
-  const { products } = useProduct([]);
+  const { productState } = useProduct();
+
   return (
     <main>
       <div className='product-grid'>
-        {products.map((pd) => (
+        {productState.products.map((pd) => (
           <ProductCard key={pd._id} product={pd} />
         ))}
       </div>
