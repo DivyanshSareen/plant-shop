73962843d700987833cff16d333246ac6f224f8f
DivyanshSareen <sareendivyansh@gmail.com>
2022-08-07 23:46:37 +0530

    chore: moved feedbackMessage() to separate file

--- src/components/Notification/notification.js
+++ src/components/Notification/notification.js
@@ -1,18 +1,7 @@
 import { Store } from "react-notifications-component";
-const feedbackMessage = (item, action) => {
-  switch (action) {
-    case "ADD_TO_CART":
-      return `${item.name.toUpperCase()} was added to CART`;
-    case "MOVE_TO_WISHLIST":
-      return `${item.name.toUpperCase()} was added to WISHLIST`;
-    case "REMOVE_FROM_CART":
-      return `${item.name.toUpperCase()} was removed from CART`;
-    case "REMOVE_FROM_WISHLIST":
-      return `${item.name.toUpperCase()} was removed from WISHLIST`;
-    default:
-      return "All set!";
-  }
-};
+
+import { feedbackMessage } from "./feedbackMessage";
+
 const notification = ({ item, action }) => {
   const feedback = feedbackMessage(item, action);
   Store.addNotification({
