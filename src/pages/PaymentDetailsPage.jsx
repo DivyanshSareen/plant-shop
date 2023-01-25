c3f93f9eafb1234a6e8b389657b2ed34efd8e961
Divyansh Sareen <sareendivyansh@gmail.com>
2023-01-25 11:33:47 +0530

    disabled eslint warnings

--- src/pages/PaymentDetailsPage.jsx
+++ src/pages/PaymentDetailsPage.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import axios from "axios";
 const FINMO_APP_KEY =
   "QUtfRklOTU9fU0JYX0VDMzMxRkU0MDg4MjRFRDNBRENCMzA5NTQ5OUFFQzY0OlNLX0ZJTk1PX1NCWF8wMjhGMTJGMl84MkYzXzQ5ODhfODIwQV8yNjA1NjMwNzY0OEI=";
 
 const PaymentDetailsPage = () => {
+  // eslint-disable-next-line
   const [checkoutId, setCheckoutId] = useState(localStorage.getItem("checkout_id"));
   const [details, setDetails] = useState({});
   useEffect(() => {
@@ -25,6 +27,7 @@ const PaymentDetailsPage = () => {
           console.log(error);
         });
     }
+    // eslint-disable-next-line
   }, []);
   return <div>Checkout ID: {details.checkoutId}</div>;
 };
